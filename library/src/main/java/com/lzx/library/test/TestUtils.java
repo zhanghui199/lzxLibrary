b4a4608b11b5ff28579fa9c23108da5e567d9562
fas_zhanghui <397243131@qq.com>
2021-07-27 16:11:19 +0800

    初始化

---
+++ library/src/main/java/com/lzx/library/test/TestUtils.java
@@ -0,0 +1,15 @@
+package com.lzx.library.test;
+
+/**
+ * <pre>
+ *     author : 张辉
+ *     e-mail : 397243131@qq.com
+ *     time   : 2021/07/27
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class TestUtils {
+
+    public final static String text1 = "测试";
+}
