652557ead4c3081ce161a0fe10cf3d505bc38cc3
fas_zhanghui <397243131@qq.com>
2021-07-29 15:02:14 +0800

    测试用列1111

--- library/src/main/java/com/lzx/library/test/TestUtils.java
+++ library/src/main/java/com/lzx/library/test/TestUtils.java
@@ -1,5 +1,7 @@
 package com.lzx.library.test;
 
+import com.google.gson.Gson;
+
 /**
  * <pre>
  *     author : 张辉
@@ -14,4 +16,9 @@ public class TestUtils {
     public final static String text1 = "测试";
 
     public final static String text2 = "测试2";
+
+
+    public void on(){
+
+    }
 }
